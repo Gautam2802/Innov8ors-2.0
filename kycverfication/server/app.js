268d5027673c7c0894c5cbf0a6b7d06abff373a9
Mehul Bansal <mehulbansal83606@gmail.com>
2024-09-28 12:00:26 +0530

    server setup

--- kycverfication/server/app.js
+++ kycverfication/server/app.js
@@ -1,6 +1,16 @@
 const express=require("express");
 const app=express();
 const port=process.env.PORT||8080;
+const connectDB=require("./config/connectDB");
+
+
+
+connectDB().then(()=>{
+    console.log("connected to db");
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 
 
